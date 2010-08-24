16f1ed757b3d62d0dd7bd1a5748f5ac0207014e0
Ian Terrell <ian.terrell@gmail.com>
2010-08-24 16:07:45 -0700

    more work at deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,83 @@
 set :application, "grumpiverse"
-set :repository,  "set your repository location here"
 
-set :scm, :git
+default_run_options[:pty] = true
+set :repository,  "git@github.com:ianterrell/grumpiverse.git"
+set :scm, "git"
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+ssh_options[:forward_agent] = true
 
-role :web, "www.grumpiverse.com"
-role :app, "www.grumpiverse.com"
-role :db,  "www.grumpiverse.com", :primary => true
+set :use_sudo,      false
 
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+role :app, "grumpiverse.com"
+role :web, "grumpiverse.com"
+role :db,  "grumpiverse.com", :primary => true
+
+set :user,          'deploy'
+set :password,      'grumpdeploy99'
+
+set :keep_releases, 10
+set :branch, "master"
+set :deploy_to,      "/home/deploy/#{application}"
+
+after "deploy:update_code","deploy:symlink_configs"
+# after "deploy:update_code","deploy:bundle_install" # Not sure how to integrate with bundler yet
+after "deploy:update_code","deploy:cleanup"
+# after "deploy:update_code","deploy:package_assets"
+
+# =============================================================================
+namespace(:deploy) do  
+  task :symlink_configs, :roles => :app, :except => {:no_symlink => true} do
+    configs = %w{ database }
+    configs.map! { |file| "ln -nfs #{shared_path}/config/#{file}.yml #{release_path}/config/#{file}.yml" }
+    run <<-CMD
+      cd #{release_path} && #{configs.join(' && ')}
+    CMD
+  end
+
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Create asset packages for production" 
+  task :package_assets, :roles => :app do
+    run <<-EOF
+     cd #{release_path} && rake asset:packager:build_all RAILS_ENV=production
+    EOF
+  end
+  
+  desc "Tail the Rails production log for this environment"
+  task :tail_production_logs, :roles => :app do
+    run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+      puts  # for an extra line break before the host name
+      puts "#{channel[:server]} -> #{data}" 
+      break if stream == :err    
+    end
+  end
+end
+
+# =============================================================================
+namespace :nginx do 
+  desc "Start Nginx on the app server."
+  task :start, :roles => :app do
+    sudo "/etc/init.d/nginx start"
+  end
+
+  desc "Restart the Nginx processes on the app server by starting and stopping the cluster."
+  task :restart , :roles => :app do
+    sudo "/etc/init.d/nginx restart"
+  end
+
+  desc "Stop the Nginx processes on the app server."
+  task :stop , :roles => :app do
+    sudo "/etc/init.d/nginx stop"
+  end
+
+  desc "Tail the nginx logs for this environment"
+  task :tail, :roles => :app do
+    run "tail -f /var/log/engineyard/nginx/vhost.access.log" do |channel, stream, data|
+      puts "#{channel[:server]}: #{data}" unless data =~ /^10\.[01]\.0/ # skips lb pull pages
+      break if stream == :err    
+    end
   end
 end
