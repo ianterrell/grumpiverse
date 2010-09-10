9446366afcefc77234b37bd0ba6df5f4e367cf74
Ian Terrell <ian.terrell@gmail.com>
2010-09-10 11:04:59 -0700

    explicitly identifying paperclip url host in production environment

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,5 +5,5 @@ PaperclipStorageHash = {
   # :bucket => Rails.application.config.s3_bucket
   
   :path => ":rails_root/public/system/:class/:attachment/:id/:style/:filename",
-  :url => "/system/:class/:attachment/:id/:style/:filename"
+  :url => "#{"http://www.grumpiverse.com" if Rails.env.production?}/system/:class/:attachment/:id/:style/:filename"
 }
