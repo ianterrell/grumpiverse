e49de2e4d81ce937c7cac704c8a8c6f82ba16e44
Ian Terrell <ian.terrell@gmail.com>
2010-09-14 14:55:28 -0700

    try 2: don't include a comic that doesn't exist, i.e. for new articles

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,2 +1,7 @@
 module Admin::BaseHelper
+  def comics_to_choose_from_for_article(article)
+    comics = Comic.recent.to_a
+    comics |= article.main_comic if article.main_comic
+    comics
+  end
 end
