5a345aafe8c5aca1ca32a4b4efc3fbe884f3623c
Ian Terrell <ian.terrell@gmail.com>
2011-01-19 18:27:18 -0800

    simple speech bubble

--- lib/comic_renderer.rb
+++ lib/comic_renderer.rb
@@ -9,7 +9,10 @@ class ComicRenderer < Svg::BaseRenderer
         svg << Character.last.svg
       end
       
-      svg.text 'some more copy', :"font-family" => 'AppleCasual', :x => 300, :y => 300, :"alignment-baseline" => "central", :"text-anchor" => "middle", :"font-size" => '24px', :"fill-opacity" => 1
+      svg.ellipse :cx => 450, :cy => 300, :rx => 100, :ry => 40, :fill => "#FFFFFF", :stroke => "#000000", :"stroke-width" => 3
+      svg.polyline :fill => "#FFFFFF", :points => "420,300 370,350 480,300", :stroke => "#000000"
+
+      svg.text 'some more copy', :"font-family" => 'AppleCasual', :x => 450, :y => 300, :"alignment-baseline" => "central", :"text-anchor" => "middle", :"font-size" => '24px', :"fill-opacity" => 1
     end
   end
 end
